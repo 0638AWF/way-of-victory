00407e0586b113b9aed61bdb1f000200b2c4185c
0638AWF <abdiwakwoya2021@gmail.com>
2024-07-13 15:55:48 +0300

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/WAY-OF-VICTORY-INTERNATIONAL-BIBLE-COLLEGE-MINSTRY/",
+  base: "/",
   build: {
     rollupOptions: {
       input: './src/main.jsx' // Add this line to specify your entry point
